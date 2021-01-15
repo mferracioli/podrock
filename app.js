4fe6cbd2cf81f9eed55712572d8e03b2337016cd
Marcelo Ferracioli <marceloferracioli@iMac-de-Marcelo.local>
2021-01-15 17:20:45 -0300

    Show spotify embed after play click.

---
+++ app.js
@@ -0,0 +1,28 @@
+var App {
+	init: function() {
+		Episodes.init();
+	}
+}
+
+var Episodes = {
+
+	init: function() {
+		this.bindEvents();
+	},
+
+	bindEvents: function() {
+		var plays = document.getElementsByClassName('play');
+		for(var i=0; i<plays.length; i++) {
+			plays[i].onclick = function(event) {
+				event.preventDefault();
+				var embed = this.parentElement.getElementsByClassName('embed')[0];
+				embed.style.display = 'block';
+				this.style.display = 'none';
+				fbq('track', 'ViewContent');
+			}
+		}
+	}
+};
+
+App.init();
+
